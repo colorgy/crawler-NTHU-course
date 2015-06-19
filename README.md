dacdff3a1a8701f76b64816b8fd2800b4f3ea188
Yukai Huang <yukaihuang1993@hotmail.com>
2015-06-19 21:30:19 +0800

    1st normal form <3

--- README.md
+++ README.md
@@ -4,3 +4,18 @@
 遵從 [Course 資料正名化](https://colorgy.hackpad.com/Courses--ZacPUB7k0tB) 規範。
 
 電腦要安裝 tesseract
+
+# 更新
+原來清大有兩個課程查詢的系統，還是照系所查詢的比較合胃口。那就重爬吧。
+
+成為花最多時間的爬蟲惹 QAQ
+
+# TODOs
+為了產生填表方便，現在是 1NF，也就是把 code 展開，做成：
+
+```
+    10410ANTH670100-ANTH104D
+    year,term,code,dep,grade
+```
+
+日後需要將其轉成 0NF，只保留 `-` 之前的部分。
--- crawler/nthu_course_crawler.rb
+++ crawler/nthu_course_crawler.rb
@@ -39,8 +39,8 @@ class NthuCourseCrawler
 
   def initialize year: current_year, term: current_term, update_progress: nil, after_each: nil, params: nil
 
-    @query_url = "https://www.ccxp.nthu.edu.tw/ccxp/INQUIRE/JH/6/6.2/6.2.9/JH629001.php"
-    @result_url = "https://www.ccxp.nthu.edu.tw/ccxp/INQUIRE/JH/6/6.2/6.2.9/JH629002.php"
+    @query_url = "https://www.ccxp.nthu.edu.tw/ccxp/INQUIRE/JH/6/6.2/6.2.3/JH623001.php"
+    @result_url = "https://www.ccxp.nthu.edu.tw/ccxp/INQUIRE/JH/6/6.2/6.2.3/JH623002.php"
 
     @year = params && params["year"].to_i || year
     @term = params && params["term"].to_i || term
@@ -56,24 +56,11 @@ class NthuCourseCrawler
 
     visit @query_url
 
-    depts = @doc.css('select[name="cou_code"] option').map {|opt| [opt[:value], opt.text]}
-    depts_h = Hash[depts[1..-1].map {|dept| [dept[0], dept[1].strip.split('　')[1].split(' ')[0]]}]
+    depts = @doc.css('select[name="DEPT"] option').map {|opt| [opt[:value], opt.text]}
+    depts_h = Hash[depts[1..-1].map {|dept| [dept[0], dept[1].strip.split('-')[1].match(/(^.+?[^\w])[\w\-\s&\.\(\)\']+$/)[1]]}]
 
-    @name_to_code = Hash[depts_h.map{|k, v| [v, k.strip]}].merge({
-      "台研教" => "GPTS",
-      "醫工所" => "BME",
-      # "不分系招生" => ,
-      "醫科系" => "DMS",
-      "IMBA碩士班" => "IMBA",
-      "EMBA專班" => "EMBA",
-      "MBA專班" => "MBA",
-      # "先進光源學位學程" =>n "NSRRC",
-      # "工工在職班" => "",
-      "學科所" => "ILS",
-      "服科所" => "ISS",
-      # "半導體專班" => ""
-    })
-    ys = "#{@year-1911}|#{@term.to_s.ljust(2, '0')}"
+    @name_to_code = Hash[depts_h.map{|k, v| [v, k.strip]}]
+    # ys = "#{@year-1911}|#{@term.to_s.ljust(2, '0')}"
 
     # fetch captcha, keep trying until correct captcha
     loop do
@@ -81,10 +68,14 @@ class NthuCourseCrawler
 
       r = RestClient.post @result_url, {
         'ACIXSTORE' => @acixstore,
-        'YS' => ys,
-        'cond' => 'b',
-        'cou_code' => depts_h.keys[0],
-        'chks' => '',
+        'T_YEAR' => (@year-1911).to_s,
+        'C_TERM' => @term.to_s.ljust(2, '0'),
+        'E_YEAR' => nil,
+        'CATEGORY' => nil,
+        'STU_CLASS' => nil,
+        'STU_GROUP' => nil,
+        'SEL_FUNC' => 'DEP',
+        'DEPT' => 'ANTH',
         'auth_num' => @captcha
       }
 
@@ -96,10 +87,14 @@ class NthuCourseCrawler
       puts depts_h[dep_c]
       r = RestClient.post @result_url, {
         'ACIXSTORE' => @acixstore,
-        'YS' => ys,
-        'cond' => 'a',
-        'cou_code' => dep_c,
-        'chks' => '',
+        'T_YEAR' => (@year-1911).to_s,
+        'C_TERM' => @term.to_s.ljust(2, '0'),
+        'E_YEAR' => nil,
+        'CATEGORY' => nil,
+        'STU_CLASS' => nil,
+        'STU_GROUP' => nil,
+        'SEL_FUNC' => 'DEP',
+        'DEPT' => dep_c,
         'auth_num' => @captcha
       }
 
@@ -122,56 +117,62 @@ class NthuCourseCrawler
   end
 
   def parse_course(doc, dep_c, dep_n)
-    rows = doc.css('tr.class3')
-    (0...rows.count).step(2) do |i|
-      datas = rows[i].css('td')
+    doc.css('div.newpage').each do |page|
+      year = nil; term = nil; department = nil; department_code = nil; class_code = nil;
 
-      lecturer = datas[5].text.strip.gsub(/\ /,'')
-      lecturer = (lecturer.scan(/^[^A-Za-z0-9]+/).first unless lecturer.scan(/^[^A-Za-z0-9]+/).empty?)
-      lecturer && lecturer.gsub!(/ /, '') && lecturer.gsub!(/ /, '')
-
-      # normalize location
-      course_days = []
-      course_periods = []
-      course_locations = []
-      datas[4].search('br').each {|br| br.replace("\n")}
-      location = datas[4].text.split("\n")[0]
-      datas[3].text.scan(/([#{DAYS.keys.join('|')}])([#{PERIODS.keys.join('|')}])/).each do |pss|
-        # [["R", "6"], ["R", "7"], ["R", "8"]]
-        course_days << DAYS[pss[0]]
-        course_periods << PERIODS[pss[1]]
-        course_locations << location
+      page.css('font').first.text.match(/(?<year>\d+)學年度第(?<term>\d)學期(?<dep>.+?)\((?<dc>[A-Z]+)\s*(?<gc>.+?)\)課程/) do |m|
+        year = m[:year]
+        term = m[:term]
+        department = m[:dep]
+        department_code = m[:dc]
+        grade_code = m[:gc]
+        class_code = "#{department_code}#{grade_code}"
       end
 
-      if @detail
-        # TODOs: parse syllabus page
-        # "https://www.ccxp.nthu.edu.tw/ccxp/INQUIRE/JH/common/Syllabus/1.php?ACIXSTORE=#{acixstore}&c_key=#{URI.encode(code)}")
-      end
+      year ||= @year-1911; term ||= @term; department ||= dep_n; department_code ||= dep_c
 
-      datas[7] && datas[7].search('br').each {|br| br.replace("\n")}
-      notes = datas[7].text.strip
+      page.css('tr:not(:first-child)').each do |row|
+        datas = row.css('td')
 
-      dep_regex = /\s*\/?((?<dep>.*?)(?<cla>\d+?[A-Z]*?))\s+?(?<type>.+?),/
-      scan_results = rows[i+1].text.scan(dep_regex)
+        lecturer = datas[5].text.strip.gsub(/\ /,'')
+        lecturer = (lecturer.scan(/^[^A-Za-z0-9]+/).first unless lecturer.scan(/^[^A-Za-z0-9]+/).empty?)
+        lecturer && lecturer.gsub!(/ /, '') && lecturer.gsub!(/ /, '')
 
-      general_code = datas[0].text.gsub(/\s+/, '')
+        # normalize location
+        course_days = []
+        course_periods = []
+        course_locations = []
+        datas[4].search('br').each {|br| br.replace("\n")}
+        location = datas[4].text.strip.split("\n")[0]
+        datas[3].text.scan(/([#{DAYS.keys.join('|')}])([#{PERIODS.keys.join('|')}])/).each do |pss|
+          # [["R", "6"], ["R", "7"], ["R", "8"]]
+          course_days << DAYS[pss[0]]
+          course_periods << PERIODS[pss[1]]
+          course_locations << location
+        end
 
-      if scan_results.empty?
-        code = general_code
+        if @detail
+          # TODOs: parse syllabus page
+          # "https://www.ccxp.nthu.edu.tw/ccxp/INQUIRE/JH/common/Syllabus/1.php?ACIXSTORE=#{acixstore}&c_key=#{URI.encode(code)}")
+        end
+
+        general_code = datas[0].text.gsub(/\s+/, '')
+
+        code = "#{year}#{term.to_s.ljust(2, '0')}#{general_code}-#{class_code}"
 
         @courses[code] || @courses[code] = {}
         @courses[code] = {
-          year: @year,
-          term: @term,
-          name: datas[1].text.strip,
+          year: year,
+          term: term,
+          name: datas[1].text.strip.gsub(/\s+/, ' '),
           code: code,
           general_code: general_code,
           credits: datas[2].text.strip.to_i,
           lecturer: lecturer,
           # notes: notes,
-          required: nil,
-          department: dep_n,
-          department_code: dep_c.strip,
+          required: datas[7] && datas[7].text.include?('必'),
+          department: department,
+          department_code: "#{department_code}#{class_code}",
           day_1: course_days[0],
           day_2: course_days[1],
           day_3: course_days[2],
@@ -200,60 +201,8 @@ class NthuCourseCrawler
           location_8: course_locations[7],
           location_9: course_locations[8],
         }
-      else
-        scan_results.each do |scan_data|
-          department = scan_data[0]
-          department_code = @name_to_code[department]
-          class_code = scan_data[1]
-          required = scan_data[2].include?("必修")
-
-          code = "#{general_code}-#{class_code}"
-
-          @courses[code] || @courses[code] = {}
-          @courses[code] = {
-            year: @year,
-            term: @term,
-            name: datas[1].text.strip,
-            code: code,
-            general_code: general_code,
-            credits: datas[2].text.strip.to_i,
-            lecturer: lecturer,
-            # notes: notes,
-            required: required,
-            department: department,
-            department_code: department_code,
-            day_1: course_days[0],
-            day_2: course_days[1],
-            day_3: course_days[2],
-            day_4: course_days[3],
-            day_5: course_days[4],
-            day_6: course_days[5],
-            day_7: course_days[6],
-            day_8: course_days[7],
-            day_9: course_days[8],
-            period_1: course_periods[0],
-            period_2: course_periods[1],
-            period_3: course_periods[2],
-            period_4: course_periods[3],
-            period_5: course_periods[4],
-            period_6: course_periods[5],
-            period_7: course_periods[6],
-            period_8: course_periods[7],
-            period_9: course_periods[8],
-            location_1: course_locations[0],
-            location_2: course_locations[1],
-            location_3: course_locations[2],
-            location_4: course_locations[3],
-            location_5: course_locations[4],
-            location_6: course_locations[5],
-            location_7: course_locations[6],
-            location_8: course_locations[7],
-            location_9: course_locations[8],
-          }
-        end
-      end
-
-    end # end each rows
+      end # each row do
+    end # each newpage do
   end # end parse_course
 
 
